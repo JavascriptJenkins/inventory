6abe08081adb2b0cafa890f122e62272c2184db8
javascriptjenkins <javascriptjenkins@gmail.com>
2024-05-07 13:14:29 -0500

    move this

--- inventory/src/main/java/com/techvvs/inventory/runlistener/GlobalRunListener.java
+++ inventory/src/main/java/com/techvvs/inventory/runlistener/GlobalRunListener.java
@@ -23,11 +23,19 @@ public class GlobalRunListener implements ApplicationListener<ApplicationReadyEv
         System.out.println("----- TechVVS Application has started ----");
         System.out.println("------- TechVVS Custom Cache Init ------");
 
-        try {
-            barcodeGenerator.generateBarcodes();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+
+        // todo: move this somewhere else
+//        try {
+//
+//            // run it 8 times 8*50 = 400
+//            for(int i = 0; i < 8; i++) {
+//                barcodeGenerator.generateBarcodes(String.valueOf(i));
+//            }
+//
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
 
         simpleCache.refreshCache();
     }
