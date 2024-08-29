61d42e95124c4d633219316173b0631ddd9fed80
javascriptjenkins <javascriptjenkins@gmail.com>
2024-08-29 14:34:51 -0500

    add a logout for when the cookie is expired.  otherwise we keep getting 403s for no reason

--- inventory/src/main/java/com/techvvs/inventory/security/JwtTokenFilter.java
+++ inventory/src/main/java/com/techvvs/inventory/security/JwtTokenFilter.java
@@ -13,6 +13,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -50,6 +51,9 @@ public class JwtTokenFilter extends OncePerRequestFilter implements CsrfTokenRep
                           //String username = jwtTokenProvider.getTokenSubject(token);
                           Authentication auth = jwtTokenProvider.getAuthentication(token);
                           SecurityContextHolder.getContext().setAuthentication(auth);
+                      } else {
+                          logger.info("token expired, logging it out. ");
+                          logout(httpServletRequest,httpServletResponse);
                       }
                   });
       }
@@ -142,4 +146,28 @@ public class JwtTokenFilter extends OncePerRequestFilter implements CsrfTokenRep
 
 
   }
+
+
+    void logout(HttpServletRequest request, HttpServletResponse response){
+
+        // Invalidate the session if it exists
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
+        // Clear authentication information from the SecurityContext
+        SecurityContextHolder.clearContext();
+
+        // Remove the JWT cookie
+        Cookie cookie = new Cookie("techvvs_token", null);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(0); // Deletes the cookie
+        response.addCookie(cookie);
+
+
+        SecurityContextHolder.getContext().setAuthentication(null); // clear the internal auth
+        SecurityContextHolder.clearContext();
+    }
 }
