299dd0d996e4e9c6c5d510d4c90aa88749e1cb49
javascriptjenkins <javascriptjenkins@gmail.com>
2024-09-13 18:36:24 -0500

    qr info

--- inventory/src/main/java/com/techvvs/inventory/security/WebSecurityConfig.java
+++ inventory/src/main/java/com/techvvs/inventory/security/WebSecurityConfig.java
@@ -51,6 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers("/login/verify").permitAll()//
             .antMatchers("/login/verify/*").permitAll()//
             .antMatchers("/login/verify/**").permitAll()//
+            .antMatchers("/qr").permitAll()//
             .antMatchers("/qr/publicinfo").permitAll()//
             .antMatchers("/qr/publicinfo/*").permitAll()//
             .antMatchers("/qr/publicinfo/**").permitAll()//
