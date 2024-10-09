5d08889fe681255cb565ffd9c4a2bd3e085b5531
javascriptjenkins <javascriptjenkins@gmail.com>
2024-10-09 15:04:15 -0500

    now we can see where the unpaid products are

--- inventory/src/main/java/com/techvvs/inventory/jparepo/ProductRepo.java
+++ inventory/src/main/java/com/techvvs/inventory/jparepo/ProductRepo.java
@@ -38,6 +38,26 @@ public interface ProductRepo extends JpaRepository<ProductVO, Integer> {
     int selectCountOfProductsRemainingInBatch(@Param("batchid") int batchid);
 
 
+    // This finds a list of pageable productVOs that are in a batchid and not paid for
+    @Query(value = "SELECT p.* " +
+            "FROM product p " +
+            "JOIN TRANSACTION_PRODUCT tp ON p.product_id = tp.productid " +
+            "JOIN TRANSACTION t ON tp.transactionid = t.transactionid " +
+            "JOIN BATCH_PRODUCT_SET bps ON tp.productid = bps.PRODUCT_SET_PRODUCT_ID " +
+            "WHERE t.PAID < t.TOTALWITHTAX " +
+            "AND bps.BATCHVO_BATCHID = :batchid",
+            countQuery = "SELECT COUNT(p.product_id) " +
+                    "FROM product p " +
+                    "JOIN TRANSACTION_PRODUCT tp ON p.product_id = tp.productid " +
+                    "JOIN TRANSACTION t ON tp.transactionid = t.transactionid " +
+                    "JOIN BATCH_PRODUCT_SET bps ON tp.productid = bps.PRODUCT_SET_PRODUCT_ID " +
+                    "WHERE t.PAID < t.TOTALWITHTAX " +
+                    "AND bps.BATCHVO_BATCHID = :batchid",
+            nativeQuery = true)
+    Page<ProductVO> findUnpaidProductsByBatchId(@Param("batchid") int batchid, Pageable pageable);
+
+
+
 
 
 //    @Query("SELECT * FROM Product p JOIN Batch b ON p.product_id = b.product_id WHERE o.customer.name = :name")
