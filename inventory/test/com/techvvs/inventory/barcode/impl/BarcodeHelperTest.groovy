1b100e5a345de056941cd1f8069cd0fc0f2ffc75
humblemouse <javascriptjenkins@gmail.com>
2024-10-16 10:47:34 -0700

    this test validates that the barcodes can handle up to 999 pages of generating unique barcodes

---
+++ inventory/test/com/techvvs/inventory/barcode/impl/BarcodeHelperTest.groovy
@@ -0,0 +1,77 @@
+package com.techvvs.inventory.barcode.impl
+
+import com.techvvs.inventory.barcode.service.BarcodeService
+import org.springframework.beans.factory.annotation.Autowired
+import spock.lang.Specification
+
+class BarcodeHelperTest extends Specification {
+
+    // mock deps
+    BarcodeService barcodeService
+
+    //create real version of what you are testing
+    BarcodeHelper barcodeHelper
+
+
+    def setup(){
+        // create mock inputs to the class
+        barcodeService = Mock(BarcodeService)
+
+
+        // create the real class
+        barcodeHelper = new BarcodeHelper(
+                barcodeService: barcodeService
+        )
+
+    }
+
+
+    def "Generate Single Barcode"() {
+        given:
+        int row = 1
+        int col = 1
+        int batchnumber = 4873985
+        int pagenumber = 1
+
+
+        when:
+        String result = barcodeHelper.generateBarcodeData(row, col, batchnumber, pagenumber)
+
+        then:
+        result == "048739801017"
+        0*_._
+    }
+
+    def "Generate Barcode with page 10"() {
+        given:
+        int row = 1
+        int col = 1
+        int batchnumber = 4873985
+        int pagenumber = 10
+
+        when:
+        String result = barcodeHelper.generateBarcodeData(row, col, batchnumber, pagenumber)
+
+        then:
+        result == "048739101018"
+        0*_._
+    }
+
+
+    def "Generate Barcode with page 100"() {
+        given:
+        int row = 1
+        int col = 1
+        int batchnumber = 4873985
+        int pagenumber = 100
+
+        when:
+        String result = barcodeHelper.generateBarcodeData(row, col, batchnumber, pagenumber)
+
+        then:
+        result == "048731001019"
+        0*_._
+    }
+
+
+}
