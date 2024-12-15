51611650fdf422c32eeffb54838131616a6c2d05
javascriptjenkins <javascriptjenkins@gmail.com>
2024-12-14 20:15:08 -0600

    menu - cache images2

--- inventory/src/main/java/com/techvvs/inventory/viewcontroller/ImageController.groovy
+++ inventory/src/main/java/com/techvvs/inventory/viewcontroller/ImageController.groovy
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.util.DigestUtils
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -59,6 +60,9 @@ class ImageController {
         HttpHeaders headers = new HttpHeaders();
         headers.setCacheControl(CacheControl.maxAge(30, TimeUnit.DAYS).cachePublic());
         headers.setLastModified(lastModifiedTime);
+        String eTag = "\"" + DigestUtils.md5DigestAsHex((productid + lastModifiedTime).getBytes()) + "\"";
+        headers.setETag(eTag);
+
 
         return ResponseEntity.ok()
                 .headers(headers)
